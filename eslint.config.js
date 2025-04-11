86f3342348ecef82267ed8a0ee533cf5cdb6c392
Sanvals <santano.sergio@gmail.com>
2025-04-11 09:51:33 +0200

    after configuring

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,16 @@
-import { defineConfig } from "eslint/config";
+import { defineConfig } from "vite";
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import react from '@vitejs/plugin-react'
 
 
 export default defineConfig([
   { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
+  {
+    base: '/sanvals/',
+    plugins: [react()]
+  }
 ]);
